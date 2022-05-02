c9a2ee14e7df13b3831991f3402da7d1bcb01dc7
Gart <gmhobson@gmail.com>
2022-05-02 16:51:50 -0500

    implemented get calls

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const db = require('../../db/connection');
 
+//view all departments
 router.get('/departments', (req, res) => {
-    const sql = `SELECT * FROM department`;;
+    const sql = `SELECT * FROM department`;
   
     db.query(sql, (err, rows) => {
       if (err) {
@@ -16,5 +17,37 @@ router.get('/departments', (req, res) => {
       });
     });
   });
+
+//view all roles
+router.get('/roles', (req, res) => {
+    const sql = `SELECT * FROM roles`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json ({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+//view all employees
+router.get('/employee', (req, res) => {
+    const sql = `SELECT * FROM employee`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json ({
+            message: 'success',
+            data: rows
+        });
+    });
+});
 
 module.exports = router;

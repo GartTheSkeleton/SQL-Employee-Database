086fd72391bad01d7ac0d4a3c81a75fa06551efc
Gart <gmhobson@gmail.com>
2022-05-04 18:38:54 -0500

    added employee update route

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use((req, res) => {
   });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    //console.log(`Server running on port ${PORT}`);
   });
 
 //inquirer questions

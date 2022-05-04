203991ad549106567d54d29d2de44021b33738c4
Gart <gmhobson@gmail.com>
2022-05-04 13:02:26 -0500

    began inquirer language

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
+const inquirer = require('inquirer');
+const axios = require('axios');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -19,3 +21,50 @@ app.use((req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
+
+//inquirer questions
+const mainMenuQuestions = [
+  {
+    type: 'list',
+    name: 'menu',
+    message: "What would you like to do?",
+    choices: ["View Employees", "View Departments", "View Roles", "Add Employee", "Add Department", "Add Role", "Update Role"]
+  }
+]
+
+let coolFunc = function(){
+  inquirer.prompt(mainMenuQuestions).then(inquirerResponses => {
+    console.log(inquirerResponses.menu);
+    let switchAnswer = inquirerResponses.switch
+    switch (switchAnswer) {
+      case "View Employees":
+          //view employees
+          break;
+      case "View Departments":
+          //view departments
+          break;
+      case "View Roles":
+          //view roles
+          break;
+      case "Add Employee":
+          //"Add Employee"
+          break;
+      case "Add Department":
+          //"Add Department"
+          break;
+      case "Add Role":
+          //"Add Role"
+          break;
+      case "Update Role":
+        //Update role
+        break;
+  }
+  })
+};
+
+
+//axios passing answers to post requests
+
+//display relevant table
+
+coolFunc();
